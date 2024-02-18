74afaaf4267fd07a332fc2761396b0ba3faf73a5
ɴᴏᴠᴀ <146671001+ovnanova@users.noreply.github.com>
2024-02-18 04:54:20 -0800

    add ANSI escape codes

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,17 @@ mod frames;
 use frames::Frames;
 
 fn main() {
+    // Clear terminal
+    print!("\x1B[2J\x1B[1;1H");
+    // Hide cursor
+    print!("\x1B[?25l");
+
     let frames = Frames::new().data;
     let animator = animator::Animator::new(frames);
     animator.run();
+
+    // Clear terminal
+    print!("\x1B[2J\x1B[1;1H");
+    // Show cursor again
+    print!("\x1B[?25h");
 }
